470d7321f0c36c952b4230b59cca661c8b06180a
João Pedro <joaoplay16@gmail.com>
2023-05-25 22:55:18 -0300

    Create BottomSheetScreen

---
+++ app/src/main/java/com/example/composematerial3/ui/screens/BottomSheetScreen.kt
@@ -0,0 +1,76 @@
+package com.example.composematerial3.ui.screens
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.composematerial3.ui.theme.ComposeMaterial3Theme
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BottomSheetScreen(
+    modifier: Modifier = Modifier
+) {
+    val scope = rememberCoroutineScope()
+    val scaffoldState = rememberBottomSheetScaffoldState()
+
+    BottomSheetScaffold(
+        modifier = modifier,
+        scaffoldState = scaffoldState,
+        sheetPeekHeight = 128.dp,
+        sheetContent = {
+            Box(
+                Modifier
+                    .fillMaxWidth()
+                    .height(128.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text("Swipe up to expand sheet")
+            }
+            Column(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(64.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text("Sheet content")
+                Spacer(Modifier.height(20.dp))
+                Button(
+                    onClick = {
+                        scope.launch { scaffoldState.bottomSheetState.partialExpand() }
+                    }
+                ) {
+                    Text("Click to collapse sheet")
+                }
+            }
+        }) { innerPadding ->
+        Box(Modifier.padding(innerPadding)) {
+            Text("Scaffold Content")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun BottomSheetScreenPreview() {
+    ComposeMaterial3Theme {
+        Surface {
+            BottomSheetScreen()
+        }
+    }
+}
