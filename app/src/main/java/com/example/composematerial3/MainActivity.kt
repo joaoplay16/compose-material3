9ca8eb78d2fc2c8ff00f7207f5ecf290e0b2b5ff
João Pedro <joaoplay16@gmail.com>
2023-05-25 22:59:24 -0300

    Create a navigation graph and set up destinations

--- app/src/main/java/com/example/composematerial3/MainActivity.kt
+++ app/src/main/java/com/example/composematerial3/MainActivity.kt
@@ -6,7 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.composematerial3.ui.screens.BottomSheetScreen
+import com.example.composematerial3.ui.screens.HomeScreen
+import com.example.composematerial3.ui.screens.Routes
 import com.example.composematerial3.ui.screens.SearchScreen
 import com.example.composematerial3.ui.theme.ComposeMaterial3Theme
 
@@ -19,9 +26,38 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    SearchScreen()
+                   DefaultNavHost()
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun DefaultNavHost() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = Routes.HOME_SCREEN.name,
+    ){
+
+        composable(Routes.HOME_SCREEN.name){
+            HomeScreen(
+                onSearchScreen = {
+                    navController.navigate(Routes.SEARCH_SCREEN.name)
+                },
+                onBottomSheetScreen = {
+                    navController.navigate(Routes.BOTTOM_SHEET_SCREEN.name)
+                }
+            )
+        }
+
+        composable(Routes.SEARCH_SCREEN.name){
+            SearchScreen()
+        }
+
+        composable(Routes.BOTTOM_SHEET_SCREEN.name){
+            BottomSheetScreen()
         }
     }
 }
